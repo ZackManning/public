430578e321702b62b31d80896be2b9c0cc719a8a
Zachary Manning <Zachary Manning>
2017-04-19 21:24:10 -0400

    Added alert when switching to another user when there are pending changes

--- PaasTheSaas/Interview/front-end/app/src/AppController.js
+++ PaasTheSaas/Interview/front-end/app/src/AppController.js
@@ -4,22 +4,23 @@
  * @param $mdSidenav
  * @constructor
  */
-function AppController(UsersDataService, $mdSidenav, $scope) {
+function AppController(UsersDataService, $mdSidenav, $scope, $mdDialog) {
   var self = this;
 
-  self.selected     = null;
-  self.users        = [ ];
-  self.selectUser   = selectUser;
-  self.toggleList   = toggleUsersList;
+  self.selected = null;
+  self.previousSelected = null;
+  self.users = [];
+  self.selectUser = selectUser;
+  self.toggleList = toggleUsersList;
 
   // Load all registered users
 
   UsersDataService
-        .loadAllUsers()
-        .then( function( users ) {
-          self.users    = [].concat(users);
-          self.selected = users[0];
-        });
+    .loadAllUsers()
+    .then(function (users) {
+      self.users = [].concat(users);
+      setSelectedUser(users[0]);
+    });
 
   // *********************************
   // Internal methods
@@ -32,14 +33,42 @@ function AppController(UsersDataService, $mdSidenav, $scope) {
     $mdSidenav('left').toggle();
   }
 
+  function setSelectedUser(user) {
+    self.selected = user;
+    self.previousSelected = angular.copy(user);
+  }
+
   /**
    * Select the current avatars
    * @param menuId
    */
-  function selectUser ( user ) {
-    console.log($scope);
-    self.selected = angular.isNumber(user) ? $scope.users[user] : user;
+  function selectUser(user) {
+    if (user === self.selected) {
+      return;
+    }
+
+    user = angular.isNumber(user) ? $scope.users[user] : user;
+
+    if (self.selected.changed) {
+      var confirm = $mdDialog.confirm()
+        .title(`User "${self.selected.name}" has changed`)
+        .textContent('Are you sure you want to switch to another user?')
+        .ariaLabel('User changed')
+        .ok('Discard changes')
+        .cancel('Go back');
+      $mdDialog.show(confirm).then(function () {
+        if (self.previousSelected) {
+          angular.copy(self.previousSelected, self.selected);
+        }
+        setSelectedUser(user);
+      }, function () {
+        // Do nothing
+      });
+    }
+    else {
+      setSelectedUser(user);
+    }
   }
 }
 
-export default [ 'UsersDataService', '$mdSidenav', '$scope', AppController ];
+export default ['UsersDataService', '$mdSidenav', '$scope', '$mdDialog', AppController];
