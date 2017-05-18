2efddd5151e13497166d82de926e6998f9062e35
Zack Manning <zmanning@gmail.com>
2017-05-18 17:30:21 -0400

    Changed the add button to always be in the lower right of the users list

--- PaasTheSaas/Interview/front-end/app/src/users/components/list/UsersList.js
+++ PaasTheSaas/Interview/front-end/app/src/users/components/list/UsersList.js
@@ -3,7 +3,7 @@ import UsersListController from './UsersListController'
 export default {
   name : 'usersList',
   config : {
-    bindings         : {  users: '<', selected: '<', showDetails: '&onSelected', addUser: '&addUser' },
+    bindings         : {  users: '<', selected: '<', showDetails: '&onSelected' },
     templateUrl      : 'src/users/components/list/UsersList.html',
     controller       : [ '$log', UsersListController ]
   }
